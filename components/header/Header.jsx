f8c5e8f22b6904d4371313d6af9572619c01460e
przekwas <luke@covalence.io>
2020-07-17 19:56:24 -0500

    bumping level up

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
 					<div className="relative mt-4 flex justify-between">
 						<div className="">
 							<span className="bg-pink-500 text-indigo-100 font-bold mt-2 px-4 py-2 rounded-full text-xs uppercase">
-								Level 1
+								Level 2
 							</span>
 						</div>
 						<div className="absolute right-0 bg-white rounded-full flex justify-center items-center p-5 border-4 border-pink-500">
